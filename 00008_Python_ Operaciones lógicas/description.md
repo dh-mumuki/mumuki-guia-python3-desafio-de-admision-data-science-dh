2363983ed7e4e59f9e5b2d1935ad499bab60e2f0
mbarone@digitalhouse.com <mbarone@digitalhouse.com>
2021-07-22 18:34:06 +0000

    Mumuki Export on 2021-07-22 18:34:06 +0000

--- 00008_Python_ Operaciones lógicas/description.md
+++ 00008_Python_ Operaciones lógicas/description.md
@@ -1,4 +1,4 @@
-Complete la función `check()` de forma tal que la función reciba un número como argumento y devuelva `True` si el número se encuentra entre 100 y 1000 (incluidos), y `False` en caso contrario.
+Complete la función `check()` de forma tal que la función reciba un número como argumento y devuelva `True` si el número se encuentra entre 100 y 1000 (inclusive), y `False` en caso contrario.
 
 La función debería cumplir las siguientes condiciones:
 
--- 00008_Python_ Operaciones lógicas/test.py
+++ 00008_Python_ Operaciones lógicas/test.py
@@ -1,28 +1,40 @@
 class TestFixtures(unittest.TestCase):
   
-  def test_Si_99_es_falso(self):
-      self.assertFalse(check(99), "check(99) debe devolver False")
+  def test_Si_10_devuelve_falso(self):
+      self.assertFalse(check(10), "check(10) debe devolver False. Revisa los límites de las condiciones.")
+      
+  def test_Si_99_devuelve_falso(self):
+      self.assertFalse(check(99), "check(99) debe devolver False. Revisa los límites de las condiciones.")
   
-  def test_1001(self):
-      self.assertFalse(check(1001), "El 1001 NO debe pasar.")
-
-  def test_900(self):
-      self.assertTrue(check(900), "El 900 debe pasar.")
-
-  def test_100(self):
-      self.assertTrue(check(100), "El 100 debe pasar.")
-
-  def test_50(self):
-      self.assertFalse(check(50), "El 50 NO debe pasar.")
+  def test_Si_100_devuelve_verdadero(self):
+      self.assertTrue(check(100), "check(100) debe devolver True. Revisa los límites de las condiciones.")
+      
+  def test_Si_101_devuelve_verdadero(self):
+      self.assertTrue(check(101), "check(101) debe devolver True. Revisa los límites de las condiciones.")
+      
+  def test_Si_387_devuelve_verdadero(self):
+      self.assertTrue(check(387), "check(387) debe devolver True. Revisa los límites de las condiciones.")
   
-  def test_n_args(self):
+  def test_Si_999_devuelve_verdadero(self):
+      self.assertTrue(check(999), "check(999) debe devolver True. Revisa los límites de las condiciones.")
+      
+  def test_Si_1000_devuelve_verdadero(self):
+      self.assertTrue(check(1000), "check(1000) debe devolver True. Revisa los límites de las condiciones.")
+      
+  def test_Si_1001_devuelve_falso(self):
+      self.assertFalse(check(1001), "check(1001) debe devolver False. Revisa los límites de las condiciones.")
+      
+  def test_Si_2749_devuelve_falso(self):
+      self.assertFalse(check(2749), "check(2749) debe devolver False. Revisa los límites de las condiciones.")   
+  
+  def test_Numero_de_argumentos(self):
     try:
       with self.assertRaises(TypeError):
         check()
       with self.assertRaises(TypeError):
         check(1, 2)
     except:
-      raise ValueError("Revisar numero de argumentos de funcion.")
+      raise ValueError("Revisar el numero de argumentos en la función.")
 
   def test_values(self):
     function = lambda x: x >= 100 and x <= 1000
